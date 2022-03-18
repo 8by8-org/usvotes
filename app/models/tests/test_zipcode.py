2814fd46f9dd16dae00271383a3aaa586c4187ec
tawong1-1834360 <tawong1@uw.edu>
2022-03-18 12:54:30 -0700

    fixed email styling, deleted postgres

--- app/models/tests/test_zipcode.py
+++
@@ -1,8 +0,0 @@
-from app.models import *
-
-def test_zipcode(app, db_session, client):
-    z = ZIPCode.find_by_zip5('66044')
-    assert len(z.counties) == 3
-
-    assert ZIPCode.guess_county('66044') == 'Douglas'
-
